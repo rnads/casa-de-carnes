128fb64561597f9a34525970cb727189427cccd5
wendellunny <wendelferreiraportugal@gmail.com>
2021-09-23 21:07:56 -0300

    Auth Layout

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 
 
 export function Login()
 {
   return (
+    <View style={styles.container}>
       <Text>Renan</Text>
+    </View>
+      
   );
 }
+
+
+const styles =  StyleSheet.create({
+  container : {
+    display: 'flex',
+    backgroundColor: '#5B0000',
+    height: '100vh'
+  }
+})

c6aa21efa341a8ca4f32ec7fc892ede94b66b121
wendellunny <wendelferreiraportugal@gmail.com>
2021-09-23 21:33:10 -0300

    Inputs

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import {StyleSheet, Text, View } from 'react-native';
-
+import { AuthLayout } from './layout/AuthLayout';
+import { TextInput } from 'react-native';
 
 export function Login()
 {
   return (
-    <View style={styles.container}>
-      <Text>Renan</Text>
-    </View>
-      
+    <AuthLayout>
+      <TextInput
+        style = {styles.input}
+        label={'Usuário'}
+        placeholder={'Digite sua email'}
+      />
+      <TextInput
+        style = {styles.input}
+        label={'Senha'}
+        placeholder={'Digite sua senha'}
+      />
+    </AuthLayout>
+    
   );
 }
 
 
-const styles =  StyleSheet.create({
-  container : {
-    display: 'flex',
-    backgroundColor: '#5B0000',
-    height: '100vh'
+const styles = StyleSheet.create({
+  input:{
+    height: '2rem',
+    backgroundColor:'#fff',
+    borderRadius:'12px',
+    width:'60%',
+    marginBottom: '1rem'
   }
-})
+
+});
+
+
+
